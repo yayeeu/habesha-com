b8753c513dc58787292f7f767045e355ad51fb3c
Yared <mrayele@gmail.com>
2025-06-08 23:55:37 +0300

    minfor fixes

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      'habesha.localhost',
+      'www.habesha.com',
+      '.habesha.localhost', // allows all subdomains of habesha.localhost
+    ],
   },
   plugins: [
     react(),

2318b2844eb79fa5abdb4d939b951c96989052a7
Yared <mrayele@gmail.com>
2025-06-11 23:18:57 +0300

    new single server build

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.post('/api/submit', async (req, res) => {
 
     // Initialize Google Sheets client
     const auth = new JWT({
-      email: process.env.GOOGLE_CLIENT_EMAIL,
-      key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      email: process.env.VITE_GOOGLE_CLIENT_EMAIL,
+      key: process.env.VITE_GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       scopes: ['https://www.googleapis.com/auth/spreadsheets'],
     });
 
@@ -43,7 +43,7 @@ app.post('/api/submit', async (req, res) => {
 
     // Append data to the sheet
     await sheets.spreadsheets.values.append({
-      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      spreadsheetId: process.env.VITE_GOOGLE_SHEET_ID,
       range: 'Sheet1!A:C',
       valueInputOption: 'USER_ENTERED',
       requestBody: {
